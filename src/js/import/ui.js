b3f394dd27b972f0d61c426d54280efb1e873db3
Tim <ktn_96@icloud.com>
2019-06-17 21:23:30 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,14 +1,28 @@
 import '../lib/selectize.min.js';
+import 'air-datepicker';
 // import '../lib/maskedinput.js';
-// import modal from 'jquery-modal';
+import modal from 'jquery-modal';
+
+
+$('.purchase__form__data input').datepicker({
+  position: 'bottom right',
+  autoClose: true,
+});
 
 //select
 $('select').selectize();
 
-// //pop-up
-// $('[rel="modal:open"]').on('click', function(event) {
-//   $(this).modal({
-//     fadeDuration: 200
-//   });
-//   return false;
-// });
+//pop-up
+$('[rel="modal:open"]').on('click', function(event) {
+  $(this).modal({
+    fadeDuration: 200
+  });
+  return false;
+});
+
+
+$('.verify-otp__input input').on('keyup', function() {
+  if($(this).val().length >= 6) {
+    $(this).blur();
+  }
+});
